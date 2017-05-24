fb00e4ac836cb184926cbdac3460010fe3c36f62
Lichun Dai <a546684355@qq.com>
2017-05-24 18:45:17 +0800

    update

--- algorithms/144-Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.js
+++ algorithms/144-Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.js
@@ -9,13 +9,58 @@
 	/**
 	 * @description preorderTraversal
 	 * @param {TreeNode} root
- 	 * @return {number[]}
+	 * @return {number[]}
 	 */
 	exports.preorderTraversal = function(root) {
 		if(!root) {
 			return [];
 		}
-		
+		var result = [];
+		var stack = [];
+		var tmp = root;
+		while(tmp || stack.length) {
+			while(tmp) {
+				result.push(tmp.val);
+				stack.push(tmp);
+				tmp = tmp.left;
+			}
+			tmp = stack.pop();
+
+			tmp = tmp.right;
+		}
+
+		return result;
+
+	};
+
+	exports.preorderTraversal2 = function(root) {
+		if(!root) {
+			return [];
+		}
+		var result = [];
+		var cur = root,
+			pre = null;
+
+		while(cur) {	
+			if(!cur.left) {
+				result.push(cur.val);
+				cur = cur.right;
+			} else {
+				pre = cur.left;
+				while(pre.right && pre.right != cur) {
+					pre = pre.right;
+				}
+				if(!pre.right) {
+					pre.right = cur;
+					result.push(cur.val);
+					cur = cur.left;
+				} else {
+					pre.right = null;
+					cur = cur.right;
+				}
+			}
+		}
+		return result;
 	};
 
 })(window.LeetCode = window.LeetCode || {});
