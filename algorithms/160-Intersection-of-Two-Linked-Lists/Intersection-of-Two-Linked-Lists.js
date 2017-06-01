09c71bbf026ea96165e3350c260721c5886b2825
Lichun Dai <a546684355@qq.com>
2017-06-01 19:06:40 +0800

    update

--- algorithms/160-Intersection-of-Two-Linked-Lists/Intersection-of-Two-Linked-Lists.js
+++ algorithms/160-Intersection-of-Two-Linked-Lists/Intersection-of-Two-Linked-Lists.js
@@ -17,7 +17,46 @@
 	 * @return {ListNode}
 	 */
 	LeetCode.getIntersectionNode = function(headA, headB) {
-
+		if(!headA || !headB) {
+			return null;
+		}
+		var lenA = 0,
+			lenB = 0;
+		var tmp = headA;
+		while(tmp) {
+			lenA ++;
+			tmp = tmp.next;
+		}
+		tmp = headB;
+		while(tmp) {
+			lenB ++;
+			tmp = tmp.next;
+		}
+		if(lenA > lenB) {
+			// 确保lenB是最长的
+			tmp = lenB;
+			lenB = lenA;
+			lenA = tmp;
+			tmp = headB;
+			headB = headA;
+			headA = tmp;
+		}
+		// lenB先走
+		tmp = lenB - lenA;
+		while(tmp) {
+			headB = headB.next;
+			tmp --;
+		}
+		// 同时走
+		while(headA && headB) {
+			if(headA.val == headB.val) {
+				return headA;
+			}
+			headA = headA.next;
+			headB = headB.next;
+		}
+		
+		return null;
 	};
 
 })(window.LeetCode = window.LeetCode || {});
