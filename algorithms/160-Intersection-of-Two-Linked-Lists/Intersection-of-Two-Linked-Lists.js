ee02ac2e1d5558f3bb83424431b8b1e27e50b33e
Lichun Dai <a546684355@qq.com>
2017-06-01 18:54:27 +0800

    update

---
+++ algorithms/160-Intersection-of-Two-Linked-Lists/Intersection-of-Two-Linked-Lists.js
@@ -0,0 +1,23 @@
+/* 作者: dailc
+ * 时间: 2017-06-01
+ * 描述: Intersection-of-Two-Linked-Lists
+ * 
+ * 来自: https://leetcode.com/problems/min-stack/
+ */
+(function(exports) {
+	function ListNode(val) {
+		this.val = val;
+		this.next = null;
+	}
+	LeetCode.ListNode = ListNode;
+	/**
+	 * @description getIntersectionNode
+	 * @param {ListNode} headA
+	 * @param {ListNode} headB
+	 * @return {ListNode}
+	 */
+	LeetCode.getIntersectionNode = function(headA, headB) {
+
+	};
+
+})(window.LeetCode = window.LeetCode || {});
