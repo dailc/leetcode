66eef868b670960e9c7606acd5cec46918f769a7
Lichun Dai <a546684355@qq.com>
2017-08-15 18:38:07 +0800

    update

---
+++ algorithms/324-Wiggle-Sort-II/README.md
@@ -0,0 +1,25 @@
+## Coin-ChangeWiggle-Sort-II
+
+## 说明
+
+给出一个未排序的数组`nums`，重排序它，使的`nums[0] < nums[1] > nums[2] < nums[3]...`
+
+例如
+
+```js
+Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6]
+
+Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2]
+```
+
+- 假设所有的输入都有合法的答案
+
+- 答案可能不止一个
+
+- `O(N)`的时间复杂度，`O(1)`的空间复杂度
+
+### 思路
+
+- 需要用到一个寻找`k`小值的算法
+
+- https://discuss.leetcode.com/topic/32929/o-n-o-1-after-median-virtual-indexing/2
---
+++ algorithms/324-Wiggle-Sort-II/Wiggle-Sort-II.js
@@ -0,0 +1,82 @@
+/* 作者: dailc
+ * 时间: 2017-08-15
+ * 描述: Wiggle-Sort-II
+ * 
+ * 来自: https://leetcode.com/problems/create-maximum-number
+ */
+(function(exports) {
+    function findK(nums, k, i, j) {
+        if (i >= j) {
+            return nums[i];
+        }
+
+        var m = partition(nums, i, j);
+
+        // 这里一直在基于m的值进行分区，有点类似于快排
+        if (m == k) {
+            return nums[m];
+        } else if (m < k) {
+            return findK(nums, k, m + 1, j);
+        } else {
+            return findK(nums, k, i, m - 1);
+        }
+    }
+
+    function partition(nums, i, j) {
+        // 确保m是当前第m小值,所以m是从0开始
+        var x = nums[i],
+            m = i,
+            n = i + 1;
+
+        while (n <= j) {
+            if (nums[n] < x) {
+                swap(nums, ++m, n);
+            }
+
+            n++;
+        }
+        // 这次的交换让m变成了第m小值
+        swap(nums, i, m);
+
+        return m;
+    }
+
+    function swap(nums, i, j) {
+        var tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    var wiggleSort = function(nums) {
+        var len = nums.length;
+
+        var midIndex = ~~(len / 2);
+
+        var mid = findK(nums, midIndex, 0, len - 1);
+        
+
+        // 3-way-partition-to-wiggly in O(n) time with O(1) space.
+        var i = 0,
+            low = 0,
+            high = len - 1;
+
+        // Index-rewiring.
+        var indexMapping = function(i) {
+            return (1 + 2*i) % (len | 1);
+        };
+        
+        
+        while (i <= high) {
+            
+            if (nums[indexMapping(i)] > mid) {
+                swap(nums, indexMapping(i++), indexMapping(low++));
+            } else if (nums[indexMapping(i)] < mid) {
+                swap(nums, indexMapping(high--), indexMapping(i));
+            } else {
+                i++;
+            }
+        }
+    };
+    exports.wiggleSort = wiggleSort;
+
+})(window.LeetCode = window.LeetCode || {});
