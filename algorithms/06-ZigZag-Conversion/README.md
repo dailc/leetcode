fdce957293b05244a78ca722ce00373efbf0c45c
Lichun Dai <a546684355@qq.com>
2017-10-23 20:20:06 +0800

    update

--- algorithms/06-ZigZag-Conversion/README.md
+++ algorithms/06-ZigZag-Conversion/README.md
@@ -1,7 +1,38 @@
-## ZigZag Conversion
+# ZigZag Conversion
+
+## 说明
+
+字符串`PAYPALISHIRING`，的之字形写法如下（给定三行）
+
+```js
+P   A   H   N
+A P L S I I G
+Y   I   R
+```
+
+上面按照列数从小往大读就是`PAHNAPLSIIGYIR`
+
+写一个方法，给出一个字符串以及行数，返回对应行数的之字形写法
 
 可参考
 http://blog.csdn.net/ljiabin/article/details/40477429
 
 
-* 理解题意最重要
+## 思路
+
+- 主要找出奇数列和偶数列的规律
+
+譬如
+
+原始字符串： `0123456789`
+
+rows=3时: `048 13579 26`
+
+rows=4时: `06 157 248 39`
+
+简单的可以用字符串数组`str[rows]`来存储每一行，最后拼接
+
+或者找规律，所有行的重复周期`2 * nRows - 2`
+
+对于首行和末行之间的行，还会额外重复一次，
+重复的这一次距离本周期起始字符的距离是 `2 * nRows - 2 - 2 * i`
--- algorithms/06-ZigZag-Conversion/zigZag-conversion.js
+++ algorithms/06-ZigZag-Conversion/zigZag-conversion.js
@@ -1,55 +1,87 @@
-/**
- * 作者: dailc
- * 时间: 2016-12-12
- * 描述: zigzag-conversion
- * 
- * 分别实现了几种方案:
- * 1.
- * 
- * 来自:https://leetcode.com/problems/zigzag-conversion/
+/* 
+ * 一刷时间: 2016-12-12
+ * 二刷时间：2017-10-23
+ * 来自: https://leetcode.com/problems/zigzag-conversion/
  */
 (function(exports) {
+    /**
+     * @param {string} s
+     * @param {number} numRows
+     * @return {string}
+     */
+    function convert(s, numRows) {
+        const len = s.length;
+        
+        if (len === 0 || numRows < 2) {
+            return s;
+        }
+        
+        // 循环周期
+        const lag = 2 * numRows - 2;
+        let res = '';
+        
+        for　(let i = 0; i < numRows; i += 1) {
+            for　(let j = i; j < len; j += lag) {
+                res += s.charAt(j);
+                
+                // 非首行和末行时还要加一个
+                if (i > 0 && i < numRows - 1) {
+                    const tmp = j + lag - 2 * i;
+                    
+                    if (tmp < len) {
+                        res += s.charAt(tmp);
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
 
-	/**
-	 * @description 
-	 * 先用最直观的方法解出来
-	 * @param {String} str
-	 * @param {Number} numRows
-	 * @return {String} 
-	 */
-	exports.convert = function(str,numRows) {
-		if(!numRows) {return str}
-		//存放临时数组的array
-		var tmpArray = [];
-		for(var i=0;i<numRows;i++) {
-			tmpArray[i] = [];
-		}
-		//为1代表row ++,否则row--
-		var delta = 1,row = 0;
-		for(var i=0,len = str.length;i<len;i++) {
-			tmpArray[row].push(str.substr(i,1));
-			row += delta;
-			if(row>numRows-1) {
-				row = numRows -2;
-				delta = -1;
-			} else if(row <0) {
-				row = 1;
-				delta = 1;
-			}
-			
-			if(row<0){
-				row = 0;
-			}else if(row>numRows-1) {
-				row = numRows -1;
-			}
-		}
-		var result = '';
-		for(var i=0;i<numRows;i++) {
-			result += tmpArray[i].join('');
-		}
-		
-		return result;
-	};
-	
+    exports.convert = convert;
+
+    /**
+     * @description 
+     * 先用最直观的方法解出来
+     * @param {String} str
+     * @param {Number} numRows
+     * @return {String} 
+     */
+    exports.convert2 = function(str, numRows) {
+        if (!numRows) {
+            return str
+        }
+        //存放临时数组的array
+        var tmpArray = [];
+        for (var i = 0; i < numRows; i++) {
+            tmpArray[i] = [];
+        }
+        //为1代表row ++,否则row--
+        var delta = 1,
+            row = 0;
+        for (var i = 0, len = str.length; i < len; i++) {
+            tmpArray[row].push(str.substr(i, 1));
+            row += delta;
+            if (row > numRows - 1) {
+                row = numRows - 2;
+                delta = -1;
+            } else if (row < 0) {
+                row = 1;
+                delta = 1;
+            }
+
+            if (row < 0) {
+                row = 0;
+            } else if (row > numRows - 1) {
+                row = numRows - 1;
+            }
+        }
+        var result = '';
+        for (var i = 0; i < numRows; i++) {
+            result += tmpArray[i].join('');
+        }
+
+        return result;
+    };
 
 })(window.LeetCode = window.LeetCode || {});
