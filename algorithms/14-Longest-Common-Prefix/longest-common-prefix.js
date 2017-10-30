83c75c195c79cce75e344ddbc1e3ef9999c8d490
Lichun Dai <a546684355@qq.com>
2017-10-30 20:09:19 +0800

    update

--- algorithms/14-Longest-Common-Prefix/longest-common-prefix.js
+++ algorithms/14-Longest-Common-Prefix/longest-common-prefix.js
@@ -5,33 +5,6 @@
  */
 (function(exports) {
 
-	/**
-	 * @description 
-	 * @param {String[]} strs
-	 * @return {String} 
-	 */
-	exports.longestCommonPrefix2 = function(strs) {
-		if(!strs || strs.length == 0) {
-			return '';
-		}
-		var prefix = strs[0],
-			size = strs.length;
-		for(var i = 1; i < size; i++) {
-			if(!prefix || !strs[i]) {
-				return '';
-			}
-			var len = prefix.length < strs[i].length ? prefix.length : strs[i].length;
-
-			var j;
-			for(j = 0; j < len; j++) {
-				if(prefix.charAt(j) != strs[i].charAt(j)) {
-					break;
-				}
-			}
-			prefix = prefix.substr(0, j);
-		}
-		return prefix;
-	};
 
 	/**
 	 * @param {string[]} strs
