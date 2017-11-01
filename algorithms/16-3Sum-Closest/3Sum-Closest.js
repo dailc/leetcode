76c2015bbbb3321ae85fc24f0ac5fa0592d5e37c
Lichun Dai <a546684355@qq.com>
2017-11-01 20:35:37 +0800

    update

--- algorithms/16-3Sum-Closest/3Sum-Closest.js
+++ algorithms/16-3Sum-Closest/3Sum-Closest.js
@@ -1,77 +1,74 @@
-/**
- * 作者: dailc
- * 时间: 2017-03-23
- * 描述: threeSumClosest
- * 
+/* 
+ * 一刷时间: 2017-03-23
+ * 二刷时间：2017-11-01
  * 来自: https://leetcode.com/problems/3sum-closest
  */
 (function(exports) {
 
-	/**
-	 * @description threeSumClosest
-	 * @param {number[]} nums 
-	 * @param {number} target 
-	 * @return {number} 
-	 */
-	exports.threeSumClosest = function(nums,target) {
-		if(!nums||nums.length<3) {
-			return null;
-		}
-		//先排序-普通的sort负数排的不对
-		nums.sort(function(a,b) {
-			return a - b;
-		});
-		console.log(JSON.stringify(nums));
-		var diff = [];
-		var len = nums.length;
-		for(var i = 0; i < len-2; i++) {
-			if (i > 0 && nums[i] == nums[i-1]) {
-				//防止重复数
-				continue;
-			}
-			var twoSum = twoSum2Closest(nums,i+1,len-1,nums[i],target);
-			if(twoSum!=null) {
-				diff.push(twoSum);				
-			}	
-		}
-		if(diff&&diff.length) {
-			var min = 0;
-			for(var i = 0; i < diff.length; i ++) {
-				if(Math.abs(diff[i])<Math.abs(diff[min])) {
-					min = i;
-				}
-			}
-			return (diff[min]+target)
-		}
-		
-		
-		return null;	
-	};
-	
-	function twoSum2Closest(nums,begin,end, target,closest ) {
-		var len = nums.length,left = begin,right = end;
-		var minSum = Math.pow(2,32)-1;
-		while(left<right){
-			var sum = nums[left]+nums[right]+target -closest;
-			//console.log("sum:"+(sum+closest));
-			if(Math.abs(sum)<=Math.abs(minSum)){
-				//只可能有一个值，并且返回的是值
-				minSum = sum;
-				//console.log("minSum:"+(minSum+closest)+"sum:"+(sum+closest));				
-			}
-			
-			if(sum>0){
-				right--;
-			}else {
-				//小于
-				left++;
-			}
-		}
-		return minSum;
-	};
-	
-	
-	
-	
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    function threeSumClosest(nums, target) {
+        if (!nums || nums.length < 3) {
+            return null;
+        }
+        nums.sort(function(a, b) {
+            return a - b;
+        });
+        const diff = [];
+        const len = nums.length;
+        
+        for (let i = 0; i < len - 2; i++) {
+            if (i > 0 && nums[i] === nums[i - 1]) {
+                // 防止重复数
+                continue;
+            }
+            
+            const twoSumCloset = twoSum2Closest(nums, i + 1, len - 1, nums[i], target);
+            
+            diff.push(twoSumCloset);
+        }
+        
+        if (diff.length) {
+            let min = 0;
+            
+            for (let i = 0; i < diff.length; i++) {
+                if (Math.abs(diff[i]) < Math.abs(diff[min])) {
+                    min = i;
+                }
+            }
+            
+            return diff[min] + target;
+        }
+        
+        return null;
+    }
+
+    exports.threeSumClosest = threeSumClosest;
+    
+    function twoSum2Closest(nums, begin, end, target, closest) {
+        const len = nums.length;
+        let left = begin;
+        let right = end;
+        let minSum = Math.pow(2, 32) - 1;
+
+        while (left < right) {
+            const sum = nums[left] + nums[right] + target - closest;
+
+            if (Math.abs(sum) <= Math.abs(minSum)) {
+                minSum = sum;
+            }
+
+            if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        return minSum;
+    }
 
 })(window.LeetCode = window.LeetCode || {});
--- algorithms/16-3Sum-Closest/README.md
+++ algorithms/16-3Sum-Closest/README.md
@@ -1,6 +1,19 @@
-## 3Sum-Closest
+# 3Sum-Closest
 
 ## 说明
-这道题和`3Sum`有所区别，这道题是求的最接近target的值，而且题目假定只有一个答案
+
+和`3Sum`有所区别，这道题是求的最接近target的值，而且题目假定只有一个答案
+
+给一个整形数组`S`，以及一个`target`，找到里面最合适的三个数，使得三个数的和最接近`target`
+
+示例
+
+```js
+S = {-1 2 1 -4},  target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+```
+
+## 思路
 
 这倒题目的解法和`3Sum`类似，只不过将`2Sum`也同步变成了`2closest`，并返回一个最接近值
