684efb39b6fe8521fb91fca749959436a828010e
Lichun Dai <a546684355@qq.com>
2017-11-04 20:48:08 +0800

    update

--- algorithms/19-Remove-Nth-Node-From-End-of-List/README.md
+++ algorithms/19-Remove-Nth-Node-From-End-of-List/README.md
@@ -1,11 +1,27 @@
-## Remove-Nth-Node-From-End-of-List
+# Remove-Nth-Node-From-End-of-List
 
 ## 说明
-删除单向链表倒数第n位的节点，并返回头节点
+
+给出一个单链表，删除倒数第n位的节点，并返回头节点
+
+示例
+
+```js
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+```
+
+注意
+
+- n永远有效
+
+- 请只遍历一遍
 
 ### 思路:
 
-* 1.通过辅助栈(压栈，出栈，然后删除对应)
-* 2.不用辅助栈，先遍历一遍找到长度，然后两个指针一起走，找到k节点
+通过辅助栈(压栈，出栈，然后删除对应)
 
-虽然从时间复杂度上来说，两者都是O(N),但实际第一种快一点
+先入栈，然后出n个，如果不够，直接返回头部，否则如果刚好最后一个是头部，返回头部
+
+否则就将目标的next置空
--- algorithms/19-Remove-Nth-Node-From-End-of-List/Remove-Nth-Node-From-End-of-List.js
+++ algorithms/19-Remove-Nth-Node-From-End-of-List/Remove-Nth-Node-From-End-of-List.js
@@ -1,110 +1,45 @@
-/**
- * 作者: dailc
- * 时间: 2017-03-25
- * 描述: Remove-Nth-Node-From-End-of-List
- * 
+/* 
+ * 一刷时间: 2017-03-25
+ * 二刷时间：2017-11-04
  * 来自: https://leetcode.com/problems/remove-nth-node-from-end-of-list
  */
 (function(exports) {
 
-	/**
-	 * @description removeNthFromEnd
-	 * @param {ListNode} head
- 	 * @param {number} n
- 	 * @return {ListNode}
-	 */
-	exports.removeNthFromEnd = function(head, n) {
-		if(!head||n<=0) {
-			return head;
-		}
-		var assistStack = [];
-		var tmp = head;
-		while(tmp) {
-			assistStack.push(tmp);
-			tmp = tmp.next;
-		}
-		var before,current;
-		while(n) {
-			current = assistStack.pop();
-			n--;
-			if(!current) {
-				//如果n超过了链表长度
-				return head;
-			}
-		}
-		before = assistStack.pop();
-		//如果前面的节点存在
-		if(before) {
-			if(before.next) {
-				before.next = before.next.next;
-			} else {
-				before.next = null;
-			}
-			
-		} else if(current){
-			//如果当前节点也存在，当前就是首节点了
-			head = current.next;
-		}
-		//当前节点不存在就代表n超过了链表长度，返回默认的head
-		
-		return head;
-		
-	};
-	
-	/**
-	 * @description 思路2，不用辅助栈
-	 * 这个虽然没有辅助栈了，但是leetcode上时间很慢
-	 * @param {ListNode} head
- 	 * @param {number} n
- 	 * @return {ListNode}
-	 */
-	exports.removeNthFromEnd2 = function(head, n) {
-		if(!head||n<=0) {
-			return head;
-		}
-		var len;
-		var tmp = head;
-		while(tmp) {
-			len++;
-			tmp = tmp.next;
-		}
-		//两个指针
-		var pSlow = head,
-			pFast = head;
-		tmp = n;
-		while(tmp&&pFast) {
-			pFast = pFast.next;
-			tmp --;
-		}
-		if(tmp) {
-			//如果没有走完，代表长度不对
-			return head;
-		}
-		var before;
-		//同时走
-		while(pFast&&pSlow) {
-			pFast = pFast.next;
-			before = pSlow;
-			pSlow = pSlow.next;
-		}
-		//这时候slow整合是模板几点
-		//如果前面的节点存在
-		if(before) {
-			if(before.next) {
-				before.next = before.next.next;
-			} else {
-				before.next = null;
-			}
-			
-		} else if(pSlow){
-			//如果当前节点也存在，当前就是首节点了
-			head = pSlow.next;
-		}
-		
-		//当前节点不存在就代表n超过了链表长度，返回默认的head
-		
-		return head;
-		
-	};
+    /**
+     * @param {ListNode} head
+     * @param {number} n
+     * @return {ListNode}
+     */
+    function removeNthFromEnd(head, n) {
+        const stack = [];
+
+        let tmp = head;
+
+        while (tmp) {
+            stack.push(tmp);
+            tmp = tmp.next;
+        }
+
+        let curr;
+        let before;
+
+        while (n) {
+            curr = stack.pop();
+
+            if (!curr) {
+                return head;
+            }
+            n--;
+        }
+        before = stack.pop();
+
+        if (before) {
+            before.next = curr.next;
+        } else if (curr) {
+            return curr.next;
+        }
+
+        return head;
+    };
 
 })(window.LeetCode = window.LeetCode || {});
