71e31695185874694acd0dd0f16b8e06f7aa8586
Lichun Dai <a546684355@qq.com>
2017-12-01 17:16:18 +0800

    update

--- algorithms/45-Jump-Game-II/Jump-Game-II.js
+++ algorithms/45-Jump-Game-II/Jump-Game-II.js
@@ -1,37 +1,43 @@
-/**
- * 作者: dailc
- * 时间: 2017-04-06
- * 描述: Jump-Game-II
- * 
+/*
+ * 一刷时间: 2017-04-06
+ * 二刷时间：2017-12-01
  * 来自: https://leetcode.com/problems/jump-game-ii
  */
 (function(exports) {
-	
-	/**
-	 * @description jump
-	 * @param {number[]} nums
- 	 * @return {number}
-	 */
-	exports.jump = function(nums) {
-		if(!nums||nums.length==0) {
-			return 0;
-		}
-		var len = nums.length;
-		var lastReach = 0;
-		var reach = 0;
-		var step = 0;
-		for( var i = 0; i <= reach && i <len; i ++ ) {
-			if(i > lastReach) {
-				step ++;
-				lastReach = reach;
-			}
-			reach = Math.max(reach,nums[i]+i)
-		}
-		if(reach < len-1) {
-			return 0;
-		}
-		return step;
-	};
-	
-	
+
+    /**
+     * 贪心算法
+     * @param {number[]} nums
+     * @return {number}
+     */
+    function jump(nums) {
+        if (!nums) {
+            return 0;
+        }
+
+        const len = nums.length;
+        let step = 0;
+        let lastReach = 0;
+        let reach = 0;
+
+        for (let i = 0; i <= reach && i < len; i++) {
+            if (i > lastReach) {
+                // 超出了步数，需更新
+                step++;
+                lastReach = reach;
+            }
+
+            reach = Math.max(reach, nums[i] + i);
+        }
+
+        if (reach < len - 1) {
+            // 无法到达，错误异常
+            return 0;
+        }
+
+        return step;
+    }
+    
+    exports.jump = jump;
+
 })(window.LeetCode = window.LeetCode || {});
