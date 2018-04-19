c49b677bf809bed3752562a020e2d8263249271e
dailc <a546684355@qq.com>
2018-04-19 15:22:31 +0800

    the solution of 101st questions

--- algorithms/101-Symmetric-Tree/Symmetric-Tree.js
+++ algorithms/101-Symmetric-Tree/Symmetric-Tree.js
@@ -1,64 +1,77 @@
-/* 作者: dailc
- * 时间: 2017-05-02
- * 描述: Symmetric-Tree
- * 
+/* 
+ * 一刷时间: 2017-05-02
+ * 二刷时间：2018-04-19
  * 来自: https://leetcode.com/problems/symmetric-tree
  */
-(function(exports) {
+(function (exports) {
 
 	/**
 	 * @description isSymmetric
 	 * @param {TreeNode} root
  	 * @return {boolean}
 	 */
-	exports.isSymmetric = function(root) {
-		if(!root) {
+	exports.isSymmetric = function (root) {
+		if (!root) {
 			return true;
 		}
-		return isSymmetricRecur(root.left,root.right);
+		return isSymmetricRecur(root.left, root.right);
 	};
-	
-	function isSymmetricRecur(left,right) {
-		if(!left&&!right) {
+
+	function isSymmetricRecur(left, right) {
+		if (!left && !right) {
 			return true;
 		}
-		if((left&&!right)||(!left&&right)||(left.val!=right.val)) {
+		if ((left && !right) || (!left && right) || (left.val != right.val)) {
 			return false;
 		}
-		
-		return isSymmetricRecur(left.left,right.right)&&isSymmetricRecur(left.right,right.left);
+
+		return isSymmetricRecur(left.left, right.right) && isSymmetricRecur(left.right, right.left);
 	}
-	
-	
-	exports.isSymmetric2 = function(root) {
-		if(!root||(!root.left&&!root.right)) {
+
+
+	/**
+ * 思路：
+ * 1. 递归的话可以轻松解决，但是考虑到会存在大量冗余的判断，效率并不高
+ * 2. 采用stack辅助，依次将树两侧的节点都对称添加进入，然后判断，如果不匹配则返回false
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+	function isSymmetric(root) {
+		if (!root || (!root.left && !root.right)) {
 			return true;
 		}
-		return help(root);
-	};
-	
-	function help(root) {
-		var stack1 = [];
-		var stack2 = [];
+
+		const stack1 = [];
+		const stack2 = [];
+
 		stack1.push(root.left);
 		stack2.push(root.right);
 
-		while(stack1.length&&stack2.length) {
-			var p = stack1.pop();
-			var q = stack2.pop();
-			if(!p && !q) {
+		while (stack1.length && stack2.length) {
+			const p = stack1.pop();
+			const q = stack2.pop();
+
+			if (!p && !q) {
 				continue;
-			} else if(!p && q || p && !q) {
-				return false;
-			} else if(p.val != q.val) {
+			}
+
+			if (!p || !q) {
 				return false;
 			}
+
+			if (p.val !== q.val) {
+				return false;
+			}
+
 			stack1.push(p.left);
 			stack2.push(q.right);
-			
-			stack2.push(p.right);
-			stack1.push(q.left);
+
+			stack1.push(p.right);
+			stack2.push(q.left);
 		}
+
 		return true;
 	}
+
+	exports.isSymmetric = isSymmetric;
 })(window.LeetCode = window.LeetCode || {});
