0d6e75144f25898a4b8248f1b45f85f62add9022
lichun.dlc <lichun.dlc@alibaba-inc.com>
2018-05-07 10:57:38 +0800

    update

--- algorithms/107-Binary-Tree-Level-Order-Traversal-II/Binary-Tree-Level-Order-Traversal-II.js
+++ algorithms/107-Binary-Tree-Level-Order-Traversal-II/Binary-Tree-Level-Order-Traversal-II.js
@@ -1,84 +1,96 @@
-/* 作者: dailc
- * 时间: 2017-05-05
- * 描述: Binary-Tree-Level-Order-Traversal-II
- * 
- * 来自: https://leetcode.com/problems/binary-tree-level-order-traversal-ii
- */
-(function(exports) {
+/*
+* 一刷时间: 2017-05-05
+* 二刷时间：2018-05-07
+* 来自: https://leetcode.com/problems/binary-tree-level-order-traversal-ii
+*/
+(function (exports) {
 	function TreeNode(val) {
-    	this.val = val;
-    	this.left = this.right = null;
-  	}
+		this.val = val;
+		this.left = this.right = null;
+	}
 	/**
 	 * @description levelOrderBottom
 	 * @param {TreeNode} root
  	 * @return {number[][]}
 	 */
-	exports.levelOrderBottom = function(root) {
-		var result = [];
-		levelOrderRecurse(result, root, 0);
-
-		return result.reverse();
-	};
-	
-	function levelOrderRecurse(result, root, level) {
-
-		if(!root) {
-			//没有就暂时不添加
-			//result[level].push(null);
-			return;
-		}
-		result[level] = result[level] || [];
-		result[level].push(root.val);
-		levelOrderRecurse(result, root.left, level + 1);
-		levelOrderRecurse(result, root.right, level + 1);
-	}
-
-	exports.levelOrderBottom2 = function(root) {
-		if(!root) {
+	function levelOrderBottom2(root) {
+		if (!root) {
 			return [];
 		}
-		var result = [];
-		var nodes = [];
+
+		const res = [];
+
+		levelOrderBottomRecurse(res, root, 0);
+
+		return res.reverse();
+	}
+
+	function levelOrderBottomRecurse(res, root, level) {
+		if (!root) {
+			return;
+		}
+		res[level] = res[level] || [];
+
+		res[level].push(root.val);
+
+		levelOrderBottomRecurse(res, root.left, level + 1);
+		levelOrderBottomRecurse(res, root.right, level + 1);
+	}
+
+	exports.levelOrderBottom2 = levelOrderBottom2;
+
+	function levelOrderBottom(root) {
+		if (!root) {
+			return [];
+		}
+
+		const res = [];
+		const nodes = [];
+
 		nodes.push({
 			node: root,
 			level: 0
 		});
-		var currLevel = -1;
-		var tmp = [];
-		while(nodes.length) {
-			//先进先出，用 shift
-			var nodeObj = nodes.shift();
-			var node = nodeObj.node;
-			var nodeLevel = nodeObj.level;
 
-			if(node.left) {
+		let currLevel = -1;
+		let tmp = [];
+
+		while (nodes.length) {
+			const nodeObj = nodes.shift();
+			const node = nodeObj.node;
+			const level = nodeObj.level;
+
+			if (node.left) {
 				nodes.push({
 					node: node.left,
-					level: nodeLevel + 1
+					level: level + 1
 				});
 			}
-			if(node.right) {
+
+			if (node.right) {
 				nodes.push({
 					node: node.right,
-					level: nodeLevel + 1
+					level: level + 1
 				});
 			}
-			if(currLevel != nodeLevel) {
-				if(currLevel!=-1) {
-					result.push(tmp);
+
+			if (currLevel !== level) {
+				if (currLevel !== -1) {
+					res.push(tmp);
 				}
-				currLevel = nodeLevel;
 				tmp = [];
+				currLevel = level;
 				tmp.push(node.val);
 			} else {
 				tmp.push(node.val);
 			}
 		}
-		result.push(tmp);
-		return result.reverse();
-	};
 
+		res.push(tmp);
 
-	
+		return res.reverse();
+	}
+
+	exports.levelOrderBottom = levelOrderBottom;
+
 })(window.LeetCode = window.LeetCode || {});
